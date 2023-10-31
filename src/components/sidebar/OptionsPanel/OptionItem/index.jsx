2adbdcb2108209361d4fd9c5dc3e1b6a6bc35d92
Usiel Solano <shebastyan@gmail.com>
2023-10-31 13:16:02 -0600

    Fix sidebar icons alignment

--- src/components/sidebar/OptionsPanel/OptionItem/index.jsx
+++ src/components/sidebar/OptionsPanel/OptionItem/index.jsx
@@ -2,26 +2,26 @@ import { useEffect, useState } from "react";
 import styles from "./OptionItem.module.css";
 
 export const OptionItem = ({ optionText, path, children }) => {
-  const [isActive, setActive] = useState(false)
+  const [isActive, setActive] = useState(false);
 
   const getActiveStyle = () => {
-    return isActive ? styles.optionPanel__itemActive : '' 
-  }
-  
-  useEffect(()=>{
-    const currentPath = window.location.pathname
+    return isActive ? styles.optionPanel__itemActive : "";
+  };
 
-    if(currentPath === path) {
-      setActive(true)
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+
+    if (currentPath === path) {
+      setActive(true);
     }
-  }, [])
+  }, []);
 
-  return(
+  return (
     <>
-      <div className={ `${styles.optionPanel__item} ${ getActiveStyle()}`}>
-        <i>{ children }</i>
-        <p>{ optionText }</p>
+      <div className={`${styles.optionPanel__item} ${getActiveStyle()}`}>
+        <i>{children}</i>
+        <p>{optionText}</p>
       </div>
     </>
-  )
-}
+  );
+};
