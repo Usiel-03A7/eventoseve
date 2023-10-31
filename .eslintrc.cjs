d4e24c31e5f2c3df27e0bb9d0d3637e989b21b09
Usiel Solano <shebastyan@gmail.com>
2023-10-31 14:17:30 -0600

    Add rules to file .esLintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true
   },
+  extends: ['standard', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 120, // Ancho máximo de línea deseado
+        tabWidth: 2// Opcional: número de espacios por tabulación
+      }
+    ]
+  }
 }
