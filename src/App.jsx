9c0348c0a06d65618d79197ab239819a6ca4ec92
Usiel Solano <shebastyan@gmail.com>
2023-12-09 20:38:19 -0600

    start the tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,97 @@
-import { Sidebar } from './components/sidebar'
-import './App.css'
-import { Scheduler } from '@aldabil/react-scheduler'
+import { Sidebar } from './components/sidebar';
+import './App.css';
+import { Scheduler } from '@aldabil/react-scheduler';
 import { Routes, Route, Outlet } from 'react-router-dom';
-import { db } from './firebase/config'
+import { db } from './firebase/config';
 import { useEffect, useState } from 'react';
 import { addDoc, collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore';
-import Inventary from './components/inventary'
+import Inventary from './components/inventary';
+
+const COSTO_POR_SILLA = 5; // Costo por silla (ajusta según tus necesidades)
+const COSTO_POR_MESA = 10; // Costo por mesa (ajusta según tus necesidades)
+
 function Layout() {
-  return <div className='container'>
-    <Sidebar />
-    <div className="container-calendar">
-      <Outlet />
-
+  return (
+    <div className='container'>
+      <Sidebar />
+      <div className="container-calendar">
+        <Outlet />
+      </div>
     </div>
-
-  </div>
+  );
 }
 
 function App() {
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+
   const getevents = () => {
     return onSnapshot(collection(db, 'eventos'), (snapshot) => {
       const documents = snapshot.docs.map((doc) => {
         const data = doc.data();
-        data.start = data.start.toDate()
-        data.end = data.end.toDate()
-        data.event_id = doc.id
-        return data
-      })
-      setEvents(documents)
-    })
-  }
+        data.start = data.start.toDate();
+        data.end = data.end.toDate();
+        data.event_id = doc.id;
+        return data;
+      });
+      setEvents(documents);
+    });
+  };
 
   useEffect(() => {
     const cancel = getevents();
-    return cancel
-  }, [])
+    return cancel;
+  }, []);
+
+  // Función para calcular el costo total
+  const calculateTotalCost = (event) => {
+    const { sillas, costoSillas, mesas, costoMesas } = event;
+    const costoSillasTotal = sillas * costoSillas;
+    const costoMesasTotal = mesas * costoMesas;
+    const costoTotal = costoSillasTotal + costoMesasTotal;
+    return costoTotal;
+  };
+
+  // Función para generar un ticket (puedes personalizar según tus necesidades)
+  const generateTicket = (event, costoTotal) => {
+    alert(`¡Ticket generado!\nEvento: ${event.title}\nCosto total: $${costoTotal}`);
+  };
 
   return (
     <Routes>
-
       <Route exact path="/" element={<Layout />} >
         <Route index element={<Scheduler
           events={events}
-          translations={
-            {
-              navigation: {
-                month: 'Mes',
-                week: 'Semana',
-                day: 'Día',
-                today: 'Hoy',
-              },
-              form: {
-                addTitle: "Añadir Evento",
-                editTitle: "Editar Evento",
-                confirm: "Confirmar",
-                cancel: "Cancelar",
-              },
-              event: {
-                allDay: "Todo el día",
-                title: "Título",
-                start: "Inicio",
-                end: "Fin",
-                description: "Descripción",
-              },
-            }}
+          translations={{
+            // ... (código existente)
+          }}
           week={{
             startHour: 0,
             endHour: 24,
           }}
           day={{
             startHour: 0,
-            endHour: 24
+            endHour: 24,
           }}
-
           onConfirm={async (event, action) => {
             console.log('ACTION', action);
             if (action === 'create') {
-              const doc = await addDoc(collection(db, 'eventos'), event)
-              event.event_id = doc.id
+              const docRef = await addDoc(collection(db, 'eventos'), event);
+              event.event_id = docRef.id;
             } else {
-              setDoc(doc(db, 'eventos', event.event_id), event)
+              setDoc(doc(db, 'eventos', event.event_id), event);
             }
-            return event
+
+            // Calcular costo total
+            const costoTotal = calculateTotalCost(event);
+
+            // Generar ticket
+            generateTicket(event, costoTotal);
+
+            return event;
           }}
           onDelete={async (event_id) => {
-            await deleteDoc(doc(db, 'eventos', event_id))
-            return event_id
+            await deleteDoc(doc(db, 'eventos', event_id));
+            return event_id;
           }}
           fields={[
             {
@@ -95,9 +100,19 @@ function App() {
               config: { label: "Sillas", required: true, min: 1, max: Infinity, variant: "outlined", decimal: true },
             },
             {
+              name: "costoSillas",
+              type: "input",
+              config: { label: "Costo por silla", required: true, variant: "outlined", decimal: true },
+            },
+            {
               name: "mesas",
               type: "input",
               config: { label: "Mesas", required: true, min: 1, max: Infinity, variant: "outlined", decimal: true },
+            },
+            {
+              name: "costoMesas",
+              type: "input",
+              config: { label: "Costo por mesa", required: true, variant: "outlined", decimal: true },
             },
             {
               name: "Información ",
@@ -109,12 +124,8 @@ function App() {
         <Route path="inventary" element={<Inventary />} />
         <Route path="newpub" element={<h1>Nueva no aplica para beta</h1>} />
       </Route>
-
     </Routes>
-
-
-
-  )
+  );
 }
 
-export default App
+export default App;
